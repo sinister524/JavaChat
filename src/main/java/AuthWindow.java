e2d1e083cd28758d7e67fa5be550ec04c2af674f
sinister524 <sinister524@gmail.com>
2019-12-13 20:48:04 +0300

    Добавил окнам методы

--- src/main/java/AuthWindow.java
+++ src/main/java/AuthWindow.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class AuthWindow extends JFrame {
     private JPanel authPanel;
@@ -7,13 +10,30 @@ public class AuthWindow extends JFrame {
     private JButton authButton;
     private JButton registrationButton;
     private JLabel loginLabel;
-    private JLabel passwordLable;
+    private JLabel passwordLabel;
     private JLabel systemInfo;
+//    private ClientController clientController;
 
     public AuthWindow() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(400, 100, 700, 800);
         setContentPane(authPanel);
+        authButton.addActionListener(e -> sendAuthTokens());
+        registrationButton.addActionListener(e -> goToRegistration());
         setVisible(true);
+    }
+
+    private void sendAuthTokens() {
+        String authTokens = "/auth " + loginField.getText() + " " + Arrays.hashCode(passwordField.getPassword());
+        // send Tokens by ClientController
+    }
+
+    private void goToRegistration() {
+        setVisible(false);
+        new RegistrationWindow();
+    }
+
+    public void setSystemInfo (String info) {
+        systemInfo.setText(info);
     }
 }
--- src/main/java/ChatWindow.java
+++ src/main/java/ChatWindow.java
@@ -1,27 +1,88 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class ChatWindow extends JFrame {
     private JPanel rootPanel;
     private JPanel centralPanel;
     private JPanel downPanel;
-    private JPanel leftPanel;
+    private JPanel rightPanel;
     private JPanel upPanel;
     private JButton sendButton;
-    private JLabel nickLabel;
     private JTextField messageField;
     private JScrollPane clientList;
     private JLabel chatLabel;
     private JScrollPane chatScroll;
-    private JTextArea textArea1;
+    private JTextArea chatMessages;
     private JLabel onlineNow;
+    private String message;
 
-    public ChatWindow() throws HeadlessException {
+    public ChatWindow() {
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
         setTitle("Chat");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(400, 100, 700, 800);
         setContentPane(rootPanel);
-        setVisible(true);
         onlineNow.setText("<html>Сейчас в<br>чате: </html>");
+        sendButton.addActionListener(e -> sendMessage());
+        messageField.addActionListener(e -> sendMessage());
+        setVisible(true);
+    }
+
+
+    private void sendMessage () {
+        message = messageField + messageField.getText();
+//        send by ClientController
+        message = null;
+        messageField.setText("");
+    }
+    private void makeMessagePrivate (String clientName) {
+        if (!message.startsWith("/w")) {
+            message = "/w " + clientName + " " + message;
+            sendButton.setText("<html>Отправить<br>" + clientName + "</html>");
+        } else {
+            message = null;
+            sendButton.setText("Отправить");
+        }
+        sendButton.revalidate();
+    }
+
+    public void createClientList (String onlineClients) {
+        String [] clients = onlineClients.split(" ");
+        rightPanel.removeAll();
+        for (int i = 1; i < clients.length; i++) {
+            JLabel clientNick = new JLabel(clients[i]);
+            clientNick.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    makeMessagePrivate(clientNick.getText());
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+            rightPanel.add(clientNick);
+            clientList.revalidate();
+        }
+    }
+    public void acceptMessage (String message) {
+        chatMessages.append(message + "\n");
     }
 }
